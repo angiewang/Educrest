c0a9cf5316876261afb0afac181ec703a8494cf3
tsumego <hughbzhang@yahoo.com>
2013-07-13 23:53:00 -0700

    HeapSort now finished

---
+++ Educrest/HeapSortViewController.h
@@ -0,0 +1,18 @@
+//
+//  BubbleSortViewController.h
+//  Educrest
+//
+//  Created by Angie Wang on 7/13/13.
+//  Copyright (c) 2013 Macroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeapSortViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIWebView *myWebView;
+@property (strong, nonatomic) IBOutlet UIWebView *heapsort;
+@property (strong, nonatomic) IBOutlet UILabel *subLabel;
+
+
+@end
--- Educrest/HeapSortViewController.m
+++ Educrest/HeapSortViewController.m
@@ -1,18 +1,18 @@
 //
-//  BubbleSortViewController.m
+//  MergeSortViewController.m
 //  Educrest
 //
 //  Created by Angie Wang on 7/13/13.
 //  Copyright (c) 2013 Macroft. All rights reserved.
 //
 
-#import "BubbleSortViewController.h"
+#import "HeapSortViewController.h"
 
-@interface BubbleSortViewController ()
+@interface HeapSortViewController ()
 
 @end
 
-@implementation BubbleSortViewController
+@implementation HeapSortViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,24 +28,24 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-//    NSString *title;
-//    NSString *image;
-
-  //  UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(25, 25, 275, 40)];
-  //  label.text = @"Bubble sort: \n This algorithm sweeps through the array, and swaps \n two elements if they are out of order.";
-    // label.lineBreakMode = UILineBreakModeWordWrap;
-   // label.numberOfLines = 0;
+    //    NSString *title;
+    //    NSString *image;
     
-  //  [self.view addSubview:label];
-    NSString *path = [[NSBundle mainBundle] pathForResource: @"bubblesort" ofType:@"gif"];
+    //  UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(25, 25, 275, 40)];
+    //  label.text = @"Bubble sort: \n This algorithm sweeps through the array, and swaps \n two elements if they are out of order.";
+    // label.lineBreakMode = UILineBreakModeWordWrap;
+    // label.numberOfLines = 0;
+    
+    //  [self.view addSubview:label];
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"heapsort" ofType:@"gif"];
     
     NSURL *url = [NSURL fileURLWithPath:path isDirectory:NO];
     /* Load the request. */
     [_myWebView loadRequest:[NSURLRequest requestWithURL:url]];
-
     
- //   _titleLabel.text = text;
-    _subLabel.text = @"Heap Sort: A heap is a structure that quickly retrieves\nits smallest element. Heapsort builds a heap\nout of the data, then uses\nthat to sort the array.";
+    
+    //   _titleLabel.text = text;
+    _subLabel.text = @"Merge Sort: an algorithm \n that is both simple and fast. \n It splits the array in half,\n recursively sorts each half,\n and then merges the two halves together.";
     _subLabel.lineBreakMode = UILineBreakModeWordWrap;
     _subLabel.numberOfLines = 0;
     [_subLabel setFont: [UIFont fontWithName: @"Helvetica Neue Light" size: 36.0]];
