a31db479e2d75b57cea748f6a0ec1947de8597e9
Angie Wang <angiewang123@gmail.com>
2013-07-14 04:44:50 -0700

    updated buttons with quiz

---
+++ Educrest/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  Educrest
+//
+//  Created by Angie Wang on 7/14/13.
+//  Copyright (c) 2013 Macroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+//@property NSURLRequest *request;
+//@property UIWebView *loadRequest;
+
+@end
---
+++ Educrest/AboutViewController.m
@@ -0,0 +1,46 @@
+//
+//  AboutViewController.m
+//  Educrest
+//
+//  Created by Angie Wang on 7/14/13.
+//  Copyright (c) 2013 Macroft. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:@"www.educrest.com"];
+//    [webView loadRequest:request];
+//}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+
+@end
