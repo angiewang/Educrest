c058ea62f3c8ce8d11b999affa2c1f949150b465
Angie Wang <angiewang123@gmail.com>
2013-07-14 22:18:39 -0700

    settings update

---
+++ Educrest/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Educrest
+//
+//  Created by Angie Wang on 7/14/13.
+//  Copyright (c) 2013 Macroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
